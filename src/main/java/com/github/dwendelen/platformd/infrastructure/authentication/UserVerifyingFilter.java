c14871578dbb33559aabda758ce8976569849095
Daan Wendelen <daanwendelen@gmail.com>
2017-01-08 14:22:38 +0100

    No long check user

--- src/main/java/com/github/dwendelen/platformd/infrastructure/authentication/UserVerifyingFilter.java
+++ src/main/java/com/github/dwendelen/platformd/infrastructure/authentication/UserVerifyingFilter.java
@@ -1,10 +1,6 @@
 package com.github.dwendelen.platformd.infrastructure.authentication;
 
 import com.github.dwendelen.platformd.core.user.User;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +10,6 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
@@ -28,7 +22,6 @@ public class UserVerifyingFilter implements Filter {
     private TokenService tokenService;
 
     private static Pattern authPattern = Pattern.compile("^/api/auth/.*$");
-    private static Pattern userPattern = Pattern.compile("^/api/users/([a-zA-Z0-9\\-]*)/?.*$");
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -61,15 +54,11 @@ public class UserVerifyingFilter implements Filter {
     }
 
     private boolean mustBeAuthenticated(HttpServletRequest request) {
-        if(matches(authPattern, request)) {
+        if(authPattern.matcher(request.getServletPath()).matches()) {
             return false;
         }
 
         return true;
-    }
-
-    private boolean matches(Pattern pattern, HttpServletRequest request) {
-        return pattern.matcher(request.getServletPath()).matches();
     }
 
     private boolean authenticate(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -82,25 +71,12 @@ public class UserVerifyingFilter implements Filter {
         try {
             User user = tokenService.parse(token);
             identityProvider.setCurrentUser(user);
-
-            return validateUserPath(request, user);
+            return true;
         } catch (Exception e) {
             logger.warn("Bad token: " + token, e);
             failed(response, "Could not authenticate user", e);
             return false;
         }
-    }
-
-    private boolean validateUserPath(HttpServletRequest request, User user) {
-        if(user.isAdmin()) {
-            return true;
-        }
-        Matcher matcher = userPattern.matcher(request.getServletPath());
-        if(!matcher.matches()) {
-            return true;
-        }
-        String userInUrl = matcher.group(1);
-        return user.getUserId().toString().equals(userInUrl);
     }
 
     private void failed(HttpServletResponse response, String reason) throws IOException {
@@ -114,7 +90,5 @@ public class UserVerifyingFilter implements Filter {
     }
 
     @Override
-    public void destroy() {
-
-    }
+    public void destroy() {}
 }
